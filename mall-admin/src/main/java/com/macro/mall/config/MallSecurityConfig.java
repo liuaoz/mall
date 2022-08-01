0b54a2d2373018e9fcacf649144e851947ccaf23
matrix <576729125@qq.com>
2022-08-01 19:49:45 +0800

    x

--- mall-admin/src/main/java/com/macro/mall/config/MallSecurityConfig.java
+++ mall-admin/src/main/java/com/macro/mall/config/MallSecurityConfig.java
@@ -19,7 +19,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * mall-security模块相关配置
- * Created by macro on 2019/11/9.
  */
 @Configuration
 @EnableWebSecurity
@@ -39,16 +38,14 @@ public class MallSecurityConfig extends SecurityConfig {
 
     @Bean
     public DynamicSecurityService dynamicSecurityService() {
-        return new DynamicSecurityService() {
-            @Override
-            public Map<String, ConfigAttribute> loadDataSource() {
-                Map<String, ConfigAttribute> map = new ConcurrentHashMap<>();
-                List<UmsResource> resourceList = resourceService.listAll();
-                for (UmsResource resource : resourceList) {
-                    map.put(resource.getUrl(), new org.springframework.security.access.SecurityConfig(resource.getId() + ":" + resource.getName()));
-                }
-                return map;
+        return () -> {
+            Map<String, ConfigAttribute> map = new ConcurrentHashMap<>();
+            List<UmsResource> resourceList = resourceService.listAll();
+            for (UmsResource resource : resourceList) {
+                map.put(resource.getUrl(), new org.springframework.security.access
+                        .SecurityConfig(resource.getId() + ":" + resource.getName()));
             }
+            return map;
         };
     }
 }
--- mall-admin/src/main/java/com/macro/mall/controller/CmsPrefrenceAreaController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/CmsPrefrenceAreaController.java
@@ -10,15 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
  * 商品优选管理Controller
- * Created by macro on 2018/6/1.
  */
-@Controller
-@Api(tags = "CmsPrefrenceAreaController", description = "商品优选管理")
+@RestController
+@Api(tags = "CmsPrefrenceAreaController")
 @RequestMapping("/prefrenceArea")
 public class CmsPrefrenceAreaController {
     @Autowired

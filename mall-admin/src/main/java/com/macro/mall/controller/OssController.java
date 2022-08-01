0020f0f24d89f97dda815f36af10adfbffaadc00
matrix <576729125@qq.com>
2022-08-01 22:15:56 +0800

    x

--- mall-admin/src/main/java/com/macro/mall/controller/OssController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/OssController.java
@@ -9,9 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,24 +17,22 @@ import javax.servlet.http.HttpServletRequest;
  * Oss对象存储管理Controller
  * Created by macro on 2018/4/26.
  */
-@Controller
-@Api(tags = "OssController", description = "Oss管理")
+@RestController
+@Api(tags = "OssController")
 @RequestMapping("/aliyun/oss")
 public class OssController {
     @Autowired
     private OssService ossService;
 
     @ApiOperation(value = "Oss上传签名生成")
-    @RequestMapping(value = "/policy", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping(value = "/policy")
     public CommonResult<OssPolicyResult> policy() {
         OssPolicyResult result = ossService.policy();
         return CommonResult.success(result);
     }
 
     @ApiOperation(value = "Oss上传成功回调")
-    @RequestMapping(value = "callback", method = RequestMethod.POST)
-    @ResponseBody
+    @PostMapping(value = "callback")
     public CommonResult<OssCallbackResult> callback(HttpServletRequest request) {
         OssCallbackResult ossCallbackResult = ossService.callback(request);
         return CommonResult.success(ossCallbackResult);
